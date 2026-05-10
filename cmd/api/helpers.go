c38262bc66c93f053ad516979c6a68723608d879
Ashvin Jangid <142579833+ashvwinn@users.noreply.github.com>
2026-05-10 17:42:10 +0530

    add common errors and helper functions

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,3 +1,29 @@
 package main
 
+import (
+	"errors"
+	"net/url"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 type envelope map[string]any
+
+func (app *application) readIntParam(c *gin.Context, param string) (int, error) {
+	id, err := strconv.Atoi(c.Param(param))
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid ID parameter")
+	}
+	return id, nil
+}
+
+func (app *application) readString(qs url.Values, key string, defaultValue string) string {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	return s
+}
