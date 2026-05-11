b5d7f9d3b1de30e2ff3c51ed1abaa9317d596b7d
Yash Pratap Kulshrestha <147393761+yash3605@users.noreply.github.com>
2026-05-11 20:48:45 +0530

    TODO: handling different IP Lookup errors

--- internal/osint/iplookup.go
+++ internal/osint/iplookup.go
@@ -8,21 +8,23 @@ import (
 	"net/http"
 )
 
-var InvalidIPRepresentation = fmt.Errorf("Invalid IP representation")
+var InvalidIPRepresentation = fmt.Errorf("Entered IP is not a Valid IP")
 
 type IPLookup struct {
-	Country     string  `json:"country"`
-	CountryCode string  `json:"countryCode"`
-	Region      string  `json:"region"`
-	RegionName  string  `json:"regionName"`
-	City        string  `json:"city"`
-	Zip         string  `json:"zip"`
-	Latitude    float32 `json:"latitude"`
-	Longitude   float32 `json:"longitude"`
-	Timezone    string  `json:"timezone"`
-	ISP         string  `json:"isp"`
-	ORG         string  `json:"org"`
-	AS          string  `json:"as"`
+	Country     string  `json:"country,omitempty"`
+	CountryCode string  `json:"countryCode,omitempty"`
+	Region      string  `json:"region,omitempty"`
+	RegionName  string  `json:"regionName,omitempty"`
+	City        string  `json:"city,omitempty"`
+	Zip         string  `json:"zip,omitempty"`
+	Latitude    float32 `json:"latitude,omitempty"`
+	Longitude   float32 `json:"longitude,omitempty"`
+	Timezone    string  `json:"timezone,omitempty"`
+	ISP         string  `json:"isp,omitempty"`
+	ORG         string  `json:"org,omitempty"`
+	AS          string  `json:"as,omitempty"`
+	Status      string  `json:"status,omitempty"`
+	Message     string  `json:"message,omitempty"`
 }
 
 func IPLookupRun(addr string) (*IPLookup, error) {
@@ -32,20 +34,20 @@ func IPLookupRun(addr string) (*IPLookup, error) {
 
 	response, err := http.Get(fmt.Sprintf("http://ip-api.com/json/%s", addr))
 	if err != nil {
-		return nil, fmt.Errorf("Failed IPLookup: %v", err)
+		return nil, fmt.Errorf("Failed in API call: %v", err)
 	}
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return nil, fmt.Errorf("Failed IPLookup: %v", err)
+		return nil, fmt.Errorf("Failed in Reading Response Body: %v", err)
 	}
 
 	var ipRes IPLookup
 	err = json.Unmarshal(body, &ipRes)
 	if err != nil {
 		// TODO: Handle error responses
-		return nil, fmt.Errorf("Failed IPLookup: %v", err)
+		return nil, fmt.Errorf("Failed in parsing response into JSON: %v", err)
 	}
 
 	return &ipRes, nil
